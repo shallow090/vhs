d33adc6b1c6b308582c8e99b6aaa025dafc670d6
Mithun <76549104+Agent-150@users.noreply.github.com>
2023-02-22 21:28:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -37,7 +37,7 @@ FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
 #start message
-START_MSG = os.environ.get("START_MESSAGE", "<b>Hey✌</b> {first}\n\n <b>I'm Super File Saver For @Ongoing_Anime_2024 </b>")
+START_MSG = os.environ.get("START_MESSAGE", "<b>Hey✌</b> {first}\n\n <b>I'm Super File Saver For @Anime_Vertix </b>")
 try:
     ADMINS=[]
     for x in (os.environ.get("ADMINS", "").split()):
